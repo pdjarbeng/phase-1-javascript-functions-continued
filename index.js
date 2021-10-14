3196c31b59bb2f6f324cfc898ffa92350ac0899c
priscilla <pdjarbeng@outlook.com>
2021-10-13 22:36:19 -0400

    commit tree fb5430c217cd21e1286d5e2ea8ce8da6ecc83016

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+function saturdayFun(target = "roller-skate") {
+    return `This Saturday, I want to ${target}!`
+}
+// saturdayFun()
+// saturdayFun("bathe my dog")
+
+function mondayWork(place = "go to the office") {
+    return `This Monday, I will ${place}.`
+}
+
+function wrapAdjective(symbol ="*"){
+    return function(rose = "special") {
+        return `You are ${symbol}${rose}${symbol}!`
+    }
+}
+
+//wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
